b25b26601cf58c3e5f25dea7cb5bc99f91af6867
brenopereira18 <brenopereira@gmail.com>
2024-11-09 16:05:01 -0300

    :sparkles: feat: adds transactions pie chart component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,6 +3,9 @@ import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
 import TimeSelect from "./_components/time-select";
+import TransactionsPieChart from "./_components/transaction-pie-chart";
+import { isMatch } from "date-fns";
+import { getDashboard } from "../_data/get-dashboard";
 
 interface HomeProps {
   searchParams: {
@@ -17,6 +20,14 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
     redirect("/login");
   }
 
+  const monthIsValid = !month || !isMatch(month, "MM");
+
+  if (monthIsValid) {
+    redirect("?month=01");
+  }
+
+  const dashboard = await getDashboard(month);
+
   return (
     <>
       <Navbar />
@@ -25,7 +36,14 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <SummaryCards month={month} />
+        <div className="grid grid-cols-[2fr,1fr]">
+          <div className="flex flex-col gap-6">
+            <SummaryCards month={month} {...dashboard} />
+            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+              <TransactionsPieChart {...dashboard} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
