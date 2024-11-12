a902a673ef7e8be3702d67a9fcf9072df7f378e3
brenopereira18 <brenopereira@gmail.com>
2024-11-12 15:36:39 -0300

    :sparkles: feat: ensure user can not create transaction on free plan if above limit

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -5,6 +5,7 @@ import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import { CheckIcon, XIcon } from "lucide-react";
 import AcquirePlanButton from "./_components/acquire-plan-button";
 import { Badge } from "../_components/ui/badge";
+import { getCurrentMonthTransactions } from "../_data/get-current-month-transactions";
 
 const SubscriptionPage = async () => {
   const { userId } = await auth();
@@ -13,6 +14,7 @@ const SubscriptionPage = async () => {
   }
 
   const user = await clerkClient().users.getUser(userId);
+  const currentMonthTransactions = await getCurrentMonthTransactions()
   const hasPremiumPlan = user.publicMetadata.subscriptionPlan == "premium";
   return (
     <>
@@ -34,7 +36,9 @@ const SubscriptionPage = async () => {
             <CardContent className="space-y-6 py-8">
               <div className="flex items-center gap-2">
                 <CheckIcon className="text-primary" />
-                <p>Apenas 10 transações por mês (7/10)</p>
+                <p>
+                  Apenas 10 transações por mês ({currentMonthTransactions}/10)
+                </p>
               </div>
               <div className="flex items-center gap-2">
                 <XIcon />
@@ -43,12 +47,14 @@ const SubscriptionPage = async () => {
             </CardContent>
           </Card>
 
-          <Card className="w-[450px]">
+          <Card className="relative w-[450px]">
             <CardHeader>
               {hasPremiumPlan && (
-                <Badge className="absolute left-4 top-4 bg-primary/10 text-primary">Ativo</Badge>
+                <Badge className="absolute left-4 top-4 bg-primary/10 text-primary">
+                  Ativo
+                </Badge>
               )}
-              <h2 className="relative text-center text-2xl font-semibold">
+              <h2 className="text-center text-2xl font-semibold">
                 Plano Premium
               </h2>
               <div className="flex items-center justify-center gap-3">
